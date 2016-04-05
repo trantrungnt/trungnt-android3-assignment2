7eda2a7119312636ca202dd8c423f85748151a41
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-06 01:23:09 +0700

    use AlertDialog

--- Student/app/src/main/java/techkids/mad3/trungnt/student/AddStudentActivity.java
+++ Student/app/src/main/java/techkids/mad3/trungnt/student/AddStudentActivity.java
@@ -1,12 +1,17 @@
 package techkids.mad3.trungnt.student;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddStudentActivity extends AppCompatActivity implements View.OnClickListener{
     Button btnSave;
+    EditText editTextName, editTextClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,44 @@ public class AddStudentActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
+        editTextName = (EditText) this.findViewById(R.id.editTextName);
+        editTextClass = (EditText) this.findViewById(R.id.editTextClass);
+
         int id = v.getId();
         if (id == R.id.btnSave)
         {
 
+            if (editTextName.getText().toString().isEmpty()
+             || editTextName.getText().length() == 0
+             || editTextClass.getText().toString().isEmpty()
+             || editTextClass.getText().length() ==0
+            )
+
+            {
+                Context context = this;
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                // set title
+                alertDialogBuilder.setTitle("WARNING ...");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("You need to type your data!")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+            }else
+            {
+                //luu du lieu hoac hien thi du lieu ....
+            }
         }
     }
 }
