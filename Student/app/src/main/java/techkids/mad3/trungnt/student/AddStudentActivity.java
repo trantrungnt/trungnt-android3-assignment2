3895703b5ffec6624089c232f1aad44f45e5836a
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-05 23:56:03 +0700

    set id for btnSave

--- Student/app/src/main/java/techkids/mad3/trungnt/student/AddStudentActivity.java
+++ Student/app/src/main/java/techkids/mad3/trungnt/student/AddStudentActivity.java
@@ -2,12 +2,26 @@ package techkids.mad3.trungnt.student;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AddStudentActivity extends AppCompatActivity {
+public class AddStudentActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+        btnSave = (Button) this.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btnSave)
+        {
+
+        }
     }
 }
